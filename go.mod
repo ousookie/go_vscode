8f97d53f2dd874ee8b1ff4e68f98ef1b8f376cab
nick <ndikanskiy@gmail.com>
2023-01-13 00:46:30 +0400

    add logic for consuming kafka messages

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module main
 
 go 1.17
+
+require github.com/confluentinc/confluent-kafka-go v1.9.2
--- main/main.go
+++ main/main.go
@@ -1,8 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	kafka "github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+type message struct {
+	time    time.Time
+	payload string
+}
+
+func printMessage(msg message) {
+	fmt.Printf("Message time_stamp: %v\nMessage payload: %v\n", msg.time, msg.payload)
+}
 
 func main() {
-	hello := "Hello world"
-	fmt.Printf("My first sentence: %v!\n", hello)
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		"group.id":          "test-consumer-group",
+		"auto.offset.reset": "earliest",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	c.SubscribeTopics([]string{"testTopic1"}, nil)
+
+	for true {
+		msg, err := c.ReadMessage(time.Second)
+		if err == nil {
+			currentMessage := message{
+				msg.Timestamp,
+				string(msg.Value),
+			}
+			fmt.Printf("Incoming message\npartition [%v]\ntime [%v]\npayload [%v]\n",
+				msg.TopicPartition.Partition,
+				currentMessage.time,
+				currentMessage.payload)
+		}
+	}
+
+	c.Close()
 }
